515ceccdca6a5fcc28f21a313e2e90fe1f1e0b78
xhu <hxtheone@gmail.com>
2016-09-01 03:10:07 +0800

    fix the homepage

--- client/components/App/component.jsx
+++ client/components/App/component.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
-
 import MessageList from '../MessageList';
+import styles from './styles';
 
 export default (props) => (
   <div>
-    <MessageList />
+    <div style = {styles.leftPanel}>
+      <p> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Create, append and reply a message </p>
+      <p> But edit or delete is not allowed </p>
+      <p> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Take care of every word you input </p>
+      <p> In a bar, what is done is done </p>
+    </div>
+    <div style = {styles.rightPanel}>
+      <MessageList />
+    </div>
   </div>
 );
--- client/components/App/styles.js
+++ client/components/App/styles.js
@@ -1,1 +1,18 @@
-export default {};
+export default {
+  leftPanel : {
+    display       : 'inline-block',
+    verticalAlign : 'top',
+    padding       : '10% 0 0 4%',
+    height        : '100%',
+    width         : '25%',
+    fontFamily    : "'Rambla', sans-serif"
+  },
+
+  rightPanel : {
+    display       : 'inline-block',
+    verticalAlign : 'top',
+    height        : '100%',
+    width         : '75%',
+    overflow      : 'auto'
+  }
+};
