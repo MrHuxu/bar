8e49757832e75c8c0d71c582681a7aefc0edbc90
MrHuxu <hxtheone@gmail.com>
2016-11-18 17:37:16 +0800

    use jquery instead of fetch

--- client/actions/PostActions.js
+++ client/actions/PostActions.js
@@ -1,3 +1,5 @@
+import $ from 'jquery';
+
 export const REFRESH_POSTS = 'REFRESH_POSTS';
 export function refreshPosts (posts) {
   return {
@@ -32,14 +34,19 @@ export function replyPost (newReply) {
 
 export function fetchPosts () {
   return function (dispatch) {
-    var request = new Request('/post/', {
-      method : 'GET'
-    });
-    fetch(request).then(res => {
-      return res.json();
-    }).then(json => {
-      if ('success' === json.result) {
-        dispatch(refreshPosts(json.posts));
+    // var request = new Request('/post/', {
+    //   method : 'GET'
+    // });
+    // fetch(request).then(res => {
+    //   return res.json();
+    // }).then(json => {
+    //   if ('success' === json.result) {
+    //     dispatch(refreshPosts(json.posts));
+    //   }
+    // });
+    $.get('/post/', (data, status, xhr) => {
+      if ('success' === data.result) {
+        dispatch(refreshPosts(data.posts));
       }
     });
   };
