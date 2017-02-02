af3ba87f56710576e9d6fb63f19f72853f153481
MrHuxu <hxtheone@gmail.com>
2017-02-02 21:30:40 +0800

    update dependencies and webpack config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -15,20 +15,16 @@ module.exports = {
   },
 
   resolve : {
-    extensions         : ['', '.jsx', '.scss', '.js', '.json'],  // along the way, subsequent file(s) to be consumed by webpack
-    modulesDirectories : [
-      'node_modules',
-      path.resolve(__dirname, './node_modules')
-    ]
+    extensions : ['.js', '.jsx', '.json', '.scss']
   },
 
   module : {
     loaders : [
-      { test: /\.(js|jsx)$/, exclude: /node_modules/, loaders: ['react-hot', 'babel'] },
-      { test: /\.css$/, loader: 'style-loader!css-loader' },
-      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&minetype=application/font-woff' },
-      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' },
-      { test: /\.(jpe?g|png|gif|svg)$/i, loader: 'url?limit=10000!img?progressive=true' }
+      { test: /\.(js|jsx)$/, exclude: /node_modules/, loaders: ['react-hot-loader', 'babel-loader'] },
+      { test: /\.css$/, loaders: ['style-loader', 'css-loader'] },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loaders: ['url-loader?limit=10000&minetype=application/font-woff'] },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loaders: ['file-loader'] },
+      { test: /\.(jpe?g|png|gif|svg)$/i, loaders: ['url-loader?limit=10000!img?progressive=true'] }
     ]
   },
 
